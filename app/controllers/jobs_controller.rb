d422126d01ffea01ed0efb170f7fa2a32c1ba004
Gary Riger <grigerny@aol.com>
2013-05-03 13:16:39 -0400

    added jobs#show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,12 +2,7 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @jobs }
-    end
+    redirect_to root_url
   end
 
   # GET /jobs/1
