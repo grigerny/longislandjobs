e18e9e5f798f43219c0d62bcb42061016651a871
Gary Riger <grigerny@aol.com>
2013-05-03 13:54:21 -0400

    fixed error explenation css

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,14 @@ class Job < ActiveRecord::Base
   attr_accessible :title, :description, :link, :contact_info
   
   validates_presence_of :title, :description, :contact_info
+  
+  before_validation :smart_add_link_protocol
+
+  protected
+
+  def smart_add_link_protocol
+    unless self.link[/^http:\/\//] || self.link[/^https:\/\//]
+      self.link = 'http://' + self.link
+    end
+  end
 end
