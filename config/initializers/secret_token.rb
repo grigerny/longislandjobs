051ca28c0e3c360af21289a80d579fd4d7d1ea7f
Gary Riger <grigerny@aol.com>
2013-05-03 08:55:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Longislandjoblink::Application.config.secret_token = '0290ee1be4408e39269a0af93297f67227f0567c615f127355951918b237bc402516943cfdbb80eed9ad66449c4f1bf810de90be0eb86ad5f0c3085949bc3b9a'
