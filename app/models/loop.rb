270afd431dfe038497aa10ff104e10e0c732a9fb
Gary Riger <grigerny@aol.com>
2013-05-11 13:44:40 -0400

    changed model

--- app/models/loop.rb
+++ app/models/loop.rb
@@ -1,3 +1,3 @@
 class Loop < ActiveRecord::Base
-  attr_accessible :firstname, :lastname, :title, :phone, :email, :website, :location, :commments, :date
+  attr_accessible :firstname, :lastname, :title, :phone, :email, :website, :location, :comment, :date
 end
