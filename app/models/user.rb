5a76838b24df135bbd9fd72c1aa5c0e9274aac9a
Gary Riger <grigerny@aol.com>
2013-05-21 21:52:31 -0400

    added token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :token
   # attr_accessible :title, :body
+  
+  before_create :generate_token
+
+   protected
+
+   def generate_token
+     begin
+       token = SecureRandom.hex(3)
+     end while User.where(:token => token).exists?
+     self.token = token
+   end
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515151207) do
+ActiveRecord::Schema.define(:version => 20130522014803) do
 
   create_table "comments", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(:version => 20130515151207) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
